66c3079fb0ad5c3d6cd3a0773b86b1ffbaf17244
jela3105 <jela3105@gmail.com>
2021-01-14 23:07:26 -0600

    Create template of swipers

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,9 +4,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hello world!!!!'),
+      appBar: AppBar(
+        title: Text('Movies at the cinema'),
+        centerTitle: false,
+        backgroundColor: Colors.redAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(children: <Widget>[_swiperCards()]),
       ),
     );
+  }
+
+  Widget _swiperCards() {
+    return Container();
   }
 }
