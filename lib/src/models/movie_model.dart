32d378c6aff79d16708c620d3f1f4c99a2dcc993
jela3105 <jela3105@gmail.com>
2021-01-24 23:34:15 -0600

    Add constructor from jsonMap

--- lib/src/models/movie_model.dart
+++ lib/src/models/movie_model.dart
@@ -1,3 +1,15 @@
+class Movies {
+  List<Movie> items = new List();
+  Movies();
+  Movies.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+    for (var item in jsonList) {
+      final movie = new Movie.fromJsonMap(item);
+      items.add(movie);
+    }
+  }
+}
+
 class Movie {
   int voteCount;
   int id;
@@ -30,4 +42,21 @@ class Movie {
     this.overview,
     this.releaseDate,
   });
+
+  Movie.fromJsonMap(Map<String, dynamic> json) {
+    voteCount = json['vote_count'];
+    id = json['id'];
+    video = json['video'];
+    voteAverage = json['vote_average'] / 1;
+    title = json['title'];
+    popularity = json['popularity'] / 1;
+    posterPath = json['poster_path'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    genreIds = json['genre_ids'].cast<int>();
+    backdropPath = json['backdrop_path'];
+    adult = json['adult'];
+    overview = json['overview'];
+    releaseDate = json['release_date'];
+  }
 }
