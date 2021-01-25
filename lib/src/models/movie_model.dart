6104bd49051b6f9672a56748038d254cfcfefdb8
jela3105 <jela3105@gmail.com>
2021-01-24 23:20:46 -0600

    Create movie model

---
+++ lib/src/models/movie_model.dart
@@ -0,0 +1,33 @@
+class Movie {
+  int voteCount;
+  int id;
+  bool video;
+  double voteAverage;
+  String title;
+  double popularity;
+  String posterPath;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String backdropPath;
+  bool adult;
+  String overview;
+  String releaseDate;
+
+  Movie({
+    this.voteCount,
+    this.id,
+    this.video,
+    this.voteAverage,
+    this.title,
+    this.popularity,
+    this.posterPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.backdropPath,
+    this.adult,
+    this.overview,
+    this.releaseDate,
+  });
+}
